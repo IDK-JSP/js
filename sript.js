e89bdb117eb289065784d101f05b09bfb0abbbcc
Martin <martin.bareteau@gmail.com>
2024-11-27 10:03:35 +0100

    adding first files

---
+++ sript.js
@@ -0,0 +1,21 @@
+console.log("j'aime peindre", "j'aimerais peindre");
+console.warn("mais cela demande beaucoup d'effort");
+console.error(
+  "Je n'ai pas assez de temps car j'ai déjà un métier, 3 passions, une femme et 4 enfants et je dois dormir la nuit"
+);
+const number1 = 99;
+const number2 = 50;
+const number3 = 1;
+const number4 = 7;
+const number5 = 3;
+const number6 = 10;
+const number7 = 8;
+const number8 = 2;
+const number9 = 1;
+const number10 = 4;
+
+const set1 = number1 - number7 - number2 + number9;
+const set2 = number10 * number6 + number7 - (number3 + number5) - number8;
+
+console.log(set1, set2);
+console.log(6 % 2, 14 % 4, 99 % 3);
